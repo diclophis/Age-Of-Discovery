61ceae072d397f9e4c486b85c39f345524623a47
Diclophis <diclophis@gmail.com>
2013-04-03 07:40:00 +0000

    fix formatting

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,4 @@
 class Hero < Neo4j::Rails::Model
-  #
   # Neo4j.rb needs to have property definitions before any validations. So, the property block needs to come before 
   # loading your devise modules. 
   #
@@ -7,54 +6,51 @@ class Hero < Neo4j::Rails::Model
   # uncomment the property definitions for those modules. Otherwise, the unused property definitions can be deleted. 
   #
   # Setup accessible attributes for your model
-   attr_accessible :email, :password, :password_confirmation, :remember_me
-  
-   property :username, :type =>   String
-   property :facebook_token, :type => String, :index => :exact
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
-   property :created_at, :type => Time
-   property :updated_at, :type => Time
+  property :username, :type =>   String
+  property :facebook_token, :type => String, :index => :exact
 
-   ## Database authenticatable
-   property :email, :type => String, :null => false, :default => "", :index => :exact
-   property :encrypted_password, :type =>  NilClass
-   
-   ## If you include devise modules, uncomment the properties below.
-    
-   ## Rememberable
-   property :remember_created_at, :type => Time
-   index :remember_token, :type => :exact
+  property :created_at, :type => Time
+  property :updated_at, :type => Time
 
-   
-   ## Recoverable
-   property :reset_password_token,   :type => NilClass, :index => :exact
-   property :reset_password_sent_at, :type =>   Time
-  
-   ## Trackable
-   property :sign_in_count, :type => Fixnum, :default => 0
-   property :current_sign_in_at, :type => Time
-   property :last_sign_in_at, :type => Time
-   property :current_sign_in_ip, :type =>  String
-   property :last_sign_in_ip, :type => String
+  ## Database authenticatable
+  property :email, :type => String, :null => false, :default => "", :index => :exact
+  property :encrypted_password, :type =>  NilClass
 
-   ## Confirmable
-   # property :confirmation_token, :type => NilClass, :index => :exact
-   # property :confirmed_at, :type => DateTime
-   # property :confirmation_sent_at, :type => DateTime
+  ## If you include devise modules, uncomment the properties below.
 
-   ## Lockable
-   #  property :failed_attempts, :type => Fixnum, :default => 0
-   # property :locked_at, :type => DateTime
-   #  property :unlock_token, :type => String, :index => :exact
+  ## Rememberable
+  property :remember_created_at, :type => Time
+  index :remember_token, :type => :exact
 
-    ## Token authenticatable
-    # property :authentication_token, :type => String, :null => true, :index => :exact
+  ## Recoverable
+  property :reset_password_token,   :type => NilClass, :index => :exact
+  property :reset_password_sent_at, :type =>   Time
+
+  ## Trackable
+  property :sign_in_count, :type => Fixnum, :default => 0
+  property :current_sign_in_at, :type => Time
+  property :last_sign_in_at, :type => Time
+  property :current_sign_in_ip, :type =>  String
+  property :last_sign_in_ip, :type => String
+
+  ## Confirmable
+  # property :confirmation_token, :type => NilClass, :index => :exact
+  # property :confirmed_at, :type => DateTime
+  # property :confirmation_sent_at, :type => DateTime
+
+  ## Lockable
+  # property :failed_attempts, :type => Fixnum, :default => 0
+  # property :locked_at, :type => DateTime
+  # property :unlock_token, :type => String, :index => :exact
+
+  ## Token authenticatable
+  # property :authentication_token, :type => String, :null => true, :index => :exact
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
 end
