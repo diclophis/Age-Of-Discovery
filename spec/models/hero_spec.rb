3e0fc9f9c663e0d234d37b6baefc0d95546a7126
Diclophis <diclophis@gmail.com>
2013-04-03 07:17:04 +0000

    repair hero spec

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -2,5 +2,12 @@
 require 'spec_helper'
 
 describe Hero do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a username, email and password" do
+    hero = Hero.new
+    hero.username = "foo"
+    hero.email = "foo@foo.com"
+    hero.password = "qwerty123"
+    hero.valid?.should be_true
+    hero.username.should == "foo"
+  end
 end
