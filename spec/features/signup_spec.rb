0f3d0492745dc56b4055024b21d51437a773221d
Diclophis <diclophis@gmail.com>
2013-04-08 07:39:23 +0000

    use routing methods not strings in feature specs

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
-describe "the signup process" do #, :type => :feature, :js => true do
+describe "the signup process" do
   before :each do
     Hero.create(:email => 'user@example.com', :password => 'password')
   end
 
   it "signs me in" do
-    visit '/hero/sign_in'
+    visit new_hero_session_path
+    #TODO: save screenshot artifacts somewhere
     page.save_screenshot('/tmp/file.png', :full => true)
     within("#new_hero") do
       fill_in 'hero_email', :with => 'user@example.com'
