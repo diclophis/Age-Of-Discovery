f6a3f575a367fc91060a725f2a78b508ad0471a3
Diclophis <diclophis@gmail.com>
2013-04-15 04:20:28 +0000

    use more specific checks for signup spec

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "the signup process" do
+describe 'the signup process' do
   before :each do
     Hero.create(:email => 'user@example.com', :password => 'password')
   end
@@ -8,12 +8,12 @@ describe "the signup process" do
   it "signs me in" do
     visit new_hero_session_path
     #TODO: save screenshot artifacts somewhere
-    page.save_screenshot('/tmp/file.png', :full => true)
-    within("#new_hero") do
-      fill_in 'hero_email', :with => 'user@example.com'
-      fill_in 'hero_password', :with => 'password'
+    #page.save_screenshot('/tmp/file.png', :full => true)
+    within('form#new_hero') do
+      find('input#hero_email').set('user@example.com')
+      find('input#hero_password').set('password')
+      find('input[name="commit"]').click
     end
-    click_button 'Sign in'
     page.should have_content 'Success'
   end
 end
