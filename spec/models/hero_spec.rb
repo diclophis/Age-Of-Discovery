570c7fc57eabd53db8b301e5365c4f7cbad53132
Diclophis <diclophis@gmail.com>
2013-04-16 09:01:43 +0000

    add rank to user model, and make user emails unique

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -11,37 +11,33 @@ describe Hero do
 
   it "should have a username, email and password" do
     hero = valid_hero
-
-    hero.valid?.should be_true
-    hero.save.should be_true
+    hero.save!.should be_nil
 
     hero.username.should == "foo"
     hero.fame.should == 0
-
     hero.converted_gold.length == 1
-    #p hero.converted_gold
-    #hero.converted_gold.sum(:worth).should == 0
 
     hero.converted_gold << Voucher.new(:worth => 10)
+    hero.converted_gold.sum(:worth).should == 10
 
     #p hero.converted_gold.methods
     #p hero.converted_gold.sum(:worth)
-    
-    hero.converted_gold.sum(:worth).should == 10
+    #p hero.converted_gold
+    #hero.converted_gold.sum(:worth).should == 0
   end
 
   it "should have a rank" do
     hero = valid_hero
     hero.rank = Rank.new
-    #hero.save.should be_true
+    hero.save!.should be_nil
   end
 
-  #it "should have a unique email" do
-  #  hero = valid_hero
-  #  hero.valid?.should be_true
-  #  hero.save.should be_true
-  #  hero_again = valid_hero
-  #  hero.save.should be_true
-  #  hero.valid?.should be_false
-  #end
+  it "should have a unique email" do
+    hero = valid_hero
+    hero.save!.should be_nil
+
+    hero_again = valid_hero
+    hero_again.valid?.should be_false
+    hero_again.save.should be_false
+  end
 end
